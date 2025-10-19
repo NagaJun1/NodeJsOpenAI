34012c5e4768e306192224f42dbf4cd57df55f9a
junpei.nagai <jumpei603@gmail.com>
2025-10-19 12:55:06 +0900

    ファイルの読み取り結果を JSON 形式で出力する様に修正

--- readFile.ts
+++ readFile.ts
@@ -5,23 +5,46 @@ import { OPEN_AI_API_KEY } from './strings';
 
 /** 画像ファイルから情報を抽出する */
 const readImgFile = async () => {
-  const fileData = fs.readFileSync('bukken1.jpg', { encoding: 'base64' });
+  const fileData = fs.readFileSync('qiita_mypage.png', { encoding: 'base64' });
   if (fileData.length < 1) {
     console.log("ファイルが読み込めません");
     return;
   }
 
   const content: ChatCompletionContentPart[] = [
-    { type: "text", text: "この画像から物件の面積・築年数を抽出してください。" },
+    { type: "text", text: "この画像から、ユーザー情報を抽出してください。" },
     { type: "image_url", image_url: { url: `data:image/png;base64,${fileData}` } }
   ];
 
   const client = new OpenAI({ apiKey: OPEN_AI_API_KEY });
   const completion = await client.chat.completions.create({
     model: "gpt-4o",
-    messages: [{ role: "user", content }]
+    messages: [{ role: "user", content }],
+
+    // ↓ 戻り値の形式定義
+    response_format: {
+      type: "json_schema",
+      json_schema: {
+        strict: true,
+        name: 'mypage_info',
+        schema: {
+          type: 'object',
+          properties: {
+            name: { type: 'string', description: 'ユーザー名' },
+            contributions: { type: 'number', description: '貢献数' },
+            postCount: { type: 'number', description: '投稿回数' },
+            followers: { type: 'number', description: 'フォロワー数' },
+            following: { type: 'number', description: 'フォロー数' },
+            introduction: { type: 'string', description: '自己紹介文' },
+          },
+          required: ['name', 'contributions', 'postCount', 'followers', 'following', 'introduction'],
+          additionalProperties: false
+        }
+      }
+    },
   });
 
+  // API からの JSON形式のレスポンスを出力
   console.log(completion.choices[0].message.content);
 };
 
